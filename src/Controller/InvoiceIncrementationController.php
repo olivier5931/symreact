2162db54751d6853560017da7b229ac76c86a0c0
Olivier5931 <olivier.looten@gmail.com>
2020-06-27 16:26:58 +0200

    Installation de l'api platform et mise en place de nos ressources

---
+++ src/Controller/InvoiceIncrementationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Invoice;
+use Doctrine\ORM\EntityManagerInterface;
+
+class InvoiceIncrementationController
+{
+	/**
+	 * @var EntityManagerInterface
+	 */
+	private $entityManager;
+
+	public function __construct(EntityManagerInterface $entityManager)
+	{
+		$this->entityManager = $entityManager;
+	}
+
+	public function __invoke(Invoice $data)
+	{
+		$data->setChrono($data->getChrono() + 1);
+
+		$this->entityManager->flush();
+
+		return $data;
+	}
+}
--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -2,109 +2,163 @@
 
 namespace App\Entity;
 
-use App\Repository\InvoiceRepository;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 
 /**
- * @ORM\Entity(repositoryClass=InvoiceRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\InvoiceRepository")
+ * @ApiResource(
+ *  subresourceOperations={
+ *      "api_customers_invoices_get_subresource"={
+ *          "normalization_context"={"groups"={"invoices_subresource"}}
+ *      }
+ *  },
+ *  itemOperations={"GET", "PUT", "DELETE", "increment"={
+ *       "method"="post",
+ *       "path"="/invoices/{id}/increment",
+ *       "controller"="App\Controller\InvoiceIncrementationController",
+ *       "swagger_context"={
+ *          "summary"="Incrémente une facture",
+ *          "description"="Incrémente le chrono d'une facture donnée"
+ *       }
+ *     }
+ *  },
+ *  attributes={
+ *      "pagination_enabled"=false,
+ *      "pagination_items_per_page"=20,
+ *      "order": {"sentAt":"desc"}
+ *  },
+ *  normalizationContext={"groups"={"invoices_read"}},
+ *  denormalizationContext={"disable_type_enforcement"=true}
+ * )
+ * @ApiFilter(OrderFilter::class, properties={"amount","sentAt"})
  */
 class Invoice
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 * @Groups({"invoices_read", "customers_read", "invoices_subresource"})
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="float")
-     */
-    private $amount;
+	/**
+	 * @ORM\Column(type="float")
+	 * @Groups({"invoices_read", "customers_read", "invoices_subresource"})
+	 * @Assert\NotBlank(message="Le montant de la facture est obligatoire !")
+	 * @Assert\Type(type="numeric", message="Le montant de la facture doit être un numérique !")
+	 */
+	private $amount;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $sentAt;
+	/**
+	 * @ORM\Column(type="datetime")
+	 * @Groups({"invoices_read", "customers_read", "invoices_subresource"})
+	 * @Assert\DateTime(message="La date doit être au format YYYY-MM-DD")
+	 * @Assert\NotBlank(message="La date d'envoi doit être renseignée")
+	 */
+	private $sentAt;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $status;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 * @Groups({"invoices_read", "customers_read", "invoices_subresource"})
+	 * @Assert\NotBlank(message="Le statut de la facture est obligatoire")
+	 * @Assert\Choice(choices={"SENT", "PAID", "CANCELLED"}, message="Le statut doit être SENT, PAID ou CANCELLED")
+	 */
+	private $status;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="invoices")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $customer;
+	/**
+	 * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="invoices")
+	 * @ORM\JoinColumn(nullable=false)
+	 * @Groups({"invoices_read"})
+	 * @Assert\NotBlank(message="Le client de la facture doit être renseigné")
+	 */
+	private $customer;
 
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $chrono;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @Groups({"invoices_read", "customers_read", "invoices_subresource"})
+	 * @Assert\NotBlank(message="Il faut absolument un chrono pour la facture")
+	 * @Assert\Type(type="integer", message="Le chrono doit être un nombre !")
+	 */
+	private $chrono;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	/**
+	 * Permet de récupérer le User à qui appartient finalement la facture
+	 * @Groups({"invoices_read", "invoices_subresource"})
+	 * @return User
+	 */
+	public function getUser(): User
+	{
+		return $this->customer->getUser();
+	}
 
-    public function getAmount(): ?float
-    {
-        return $this->amount;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function setAmount(float $amount): self
-    {
-        $this->amount = $amount;
+	public function getAmount(): ?float
+	{
+		return $this->amount;
+	}
 
-        return $this;
-    }
+	public function setAmount($amount): self
+	{
+		$this->amount = $amount;
 
-    public function getSentAt(): ?\DateTimeInterface
-    {
-        return $this->sentAt;
-    }
+		return $this;
+	}
 
-    public function setSentAt(\DateTimeInterface $sentAt): self
-    {
-        $this->sentAt = $sentAt;
+	public function getSentAt(): ?\DateTimeInterface
+	{
+		return $this->sentAt;
+	}
 
-        return $this;
-    }
+	public function setSentAt($sentAt): self
+	{
+		$this->sentAt = $sentAt;
 
-    public function getStatus(): ?string
-    {
-        return $this->status;
-    }
+		return $this;
+	}
 
-    public function setStatus(string $status): self
-    {
-        $this->status = $status;
+	public function getStatus(): ?string
+	{
+		return $this->status;
+	}
 
-        return $this;
-    }
+	public function setStatus(string $status): self
+	{
+		$this->status = $status;
 
-    public function getCustomer(): ?Customer
-    {
-        return $this->customer;
-    }
+		return $this;
+	}
 
-    public function setCustomer(?Customer $customer): self
-    {
-        $this->customer = $customer;
+	public function getCustomer(): ?Customer
+	{
+		return $this->customer;
+	}
 
-        return $this;
-    }
+	public function setCustomer(?Customer $customer): self
+	{
+		$this->customer = $customer;
 
-    public function getChrono(): ?int
-    {
-        return $this->chrono;
-    }
+		return $this;
+	}
 
-    public function setChrono(int $chrono): self
-    {
-        $this->chrono = $chrono;
+	public function getChrono(): ?int
+	{
+		return $this->chrono;
+	}
 
-        return $this;
-    }
+	public function setChrono($chrono): self
+	{
+		$this->chrono = $chrono;
+
+		return $this;
+	}
 }
